ae260e74e0c14371e3df992946da8ad103e6b745
Łukasz Pitrus <l.pitrus@praktyki.ideo.pl>
2021-10-30 23:13:33 +0200

    init branch

--- yaptide/simulation_runner/shieldhit_runner.py
+++ yaptide/simulation_runner/shieldhit_runner.py
@@ -10,7 +10,10 @@ import tempfile
 import pathlib
 
 from pymchelper.executor.options import SimulationSettings
-from pymchelper.executor.runner import OutputDataType, Runner
+from pymchelper.executor.runner import Runner as SHRunner
+
+# from ..converter.converter.converter import DummmyParser
+# from ..converter.converter.converter import Runner as ConvertRunner
 
 input_cfg_templ = {}
 input_cfg_templ['beam.dat'] = """
@@ -65,10 +68,8 @@ input_cfg_templ['geo.dat'] = """
 """
 
 
-def run_shieldhit(param_dict):
+def run_shieldhit(param_dict, json_to_convert):
     """Shieldhit runner"""
-    import pymchelper
-
     input_dict = input_cfg_templ.copy()
 
     input_dict['beam.dat'] = input_dict['beam.dat'].format(
@@ -87,21 +88,28 @@ def run_shieldhit(param_dict):
     # create temporary directory
     with tempfile.TemporaryDirectory() as tmp_output_path:
 
+        # convert_runner = ConvertRunner(parser=DummmyParser(),
+        #                                input_data=json_to_convert,
+        #                                output_dir=tmp_output_path)
+
+        # convert_runner.run_parser()
+
         for config_filename in input_dict:
             abs_input_path = os.path.join(tmp_output_path, config_filename)
             with open(abs_input_path, 'w') as temp_input_file:
                 temp_input_file.write(input_dict[config_filename])
 
+        print(os.listdir(tmp_output_path))
         settings = SimulationSettings(input_path=tmp_output_path,
                                       simulator_exec_path=None,
                                       cmdline_opts='')
 
-        runner_obj = Runner(jobs=param_dict['jobs'],
-                            keep_workspace_after_run=False,
-                            output_directory=tmp_output_path)
+        runner_obj = SHRunner(jobs=param_dict['jobs'],
+                              keep_workspace_after_run=False,
+                              output_directory=tmp_output_path)
 
         start_time = timeit.default_timer()
-        isRunOk = runner_obj.run(settings=settings)
+        isRunOk = False     # runner_obj.run(settings=settings)
         if not isRunOk:
             return None
 
@@ -110,8 +118,4 @@ def run_shieldhit(param_dict):
 
         estimator = runner_obj.get_data()
 
-        return estimator
-
-
-if __name__ == '__main__':
-    sys.exit(run_shieldhit(sys.argv[1:]))
+        return estimator['mesh_']
