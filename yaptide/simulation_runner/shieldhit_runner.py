48f77e6afacc957ba641d053fab84522300e193a
Łukasz Pitrus <l.pitrus@praktyki.ideo.pl>
2021-10-31 13:04:10 +0100

    minor changes

--- yaptide/simulation_runner/shieldhit_runner.py
+++ yaptide/simulation_runner/shieldhit_runner.py
@@ -13,16 +13,16 @@ from pymchelper.executor.options import SimulationSettings
 from pymchelper.executor.runner import Runner as SHRunner
 
 from ..converter.converter.converter import DummmyParser
-from ..converter.converter.converter import Runner as ConvertRunner
+from ..converter.converter.converter import Runner as ConvRunner
 
 
-def run_shieldhit(param_dict, json_to_convert):
+def run_shieldhit(param_dict, json_to_conv):
     """Shieldhit runner"""
     # create temporary directory
     with tempfile.TemporaryDirectory() as tmp_output_path:
 
-        convert_runner = ConvertRunner(parser=DummmyParser(),
-                                       input_data=json_to_convert,
+        convert_runner = ConvRunner(parser=DummmyParser(),
+                                       input_data=json_to_conv,
                                        output_dir=tmp_output_path)
 
         convert_runner.run_parser()
@@ -37,8 +37,7 @@ def run_shieldhit(param_dict, json_to_convert):
                               output_directory=tmp_output_path)
 
         start_time = timeit.default_timer()
-        isRunOk = runner_obj.run(settings=settings)
-        if not isRunOk:
+        if not runner_obj.run(settings=settings):
             return None
 
         elapsed = timeit.default_timer() - start_time
