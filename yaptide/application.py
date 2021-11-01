58a91583d542b82681763eb8f859a83d8bbec8e9
Łukasz Pitrus <l.pitrus@praktyki.ideo.pl>
2021-11-01 20:45:59 +0100

    deep

--- yaptide/application.py
+++ yaptide/application.py
@@ -21,7 +21,8 @@ def create_app(config_object="yaptide.settings"):
     @app.after_request
     def after_request(response):
         response.headers.add('Access-Control-Allow-Origin', '*')
-        response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+        response.headers.add('Access-Control-Allow-Headers',
+                             'Content-Type,Authorization')
         response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
         return response
 
