b3a5f238f5385111f182db39d0c45779ef5423f2
Łukasz Pitrus <l.pitrus@praktyki.ideo.pl>
2021-11-25 01:24:21 +0100

    Update utils.py

--- yaptide/utils.py
+++ yaptide/utils.py
@@ -6,19 +6,19 @@ import jwt
 
 SECRET_KEY_TOKEN = token_hex(64)
 SECRET_KEY_TOKEN_REFRESH = token_hex(64)
-Refresh_Token_Expiration_Time = 7200
-Token_Expiration_Time = 600
+_Refresh_Token_Expiration_Time = 120
+_Access_Token_Expiration_Time = 10
 
 
 def encode_auth_token(user_id: int, isRefresh: bool = False) -> tuple[Union[str, Exception], datetime]:
     """Function encoding the token"""
     if isRefresh:
         secret = SECRET_KEY_TOKEN_REFRESH
-        exp_time_minutes = Refresh_Token_Expiration_Time
+        exp_time_minutes = _Refresh_Token_Expiration_Time
     else:
         secret = SECRET_KEY_TOKEN
-        exp_time_minutes = Token_Expiration_Time
-    exp = datetime.utcnow() + timedelta(seconds=exp_time_minutes)
+        exp_time_minutes = _Access_Token_Expiration_Time
+    exp = datetime.utcnow() + timedelta(minutes=exp_time_minutes)
     try:
         payload = {
             'exp': exp,
