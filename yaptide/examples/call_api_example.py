479bdee26ba7a5508227c584e95d40fa8c2ba356
Łukasz Pitrus <56313631+p1003@users.noreply.github.com>
2021-12-08 13:06:24 +0100

    Refactor routes' names (#155)
    * .
    
    * Update call_api_example.py
    
    Co-authored-by: Łukasz Pitrus <l.pitrus@praktyki.ideo.pl>

--- yaptide/examples/call_api_example.py
+++ yaptide/examples/call_api_example.py
@@ -10,7 +10,7 @@ def run_simulation_on_backend():
     http_sim_run = 'http://localhost:5000/sh/run?sim_type=sh_dummy'
     http_sim_status = 'http://localhost:5000/sh/status'
     http_sim_inputs = 'http://localhost:5000/sh/inputs'
-    http_list_sims = 'http://localhost:5000/sh/list_sims'
+    http_list_sims = 'http://localhost:5000/user/simulations'
 
     http_auth_register = 'http://localhost:5000/auth/register'
     http_auth_login = 'http://localhost:5000/auth/login'
