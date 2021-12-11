d9f0cb21715ded87676468eb56b38b239b50f84a
Łukasz Pitrus <l.pitrus@praktyki.ideo.pl>
2021-12-11 15:51:24 +0100

    Update response_templates.py

--- yaptide/routes/utils/response_templates.py
+++ yaptide/routes/utils/response_templates.py
@@ -5,7 +5,9 @@ from flask import make_response, Response
 def yaptide_response(message: str, code: int, content: Union[dict, str] = "") -> Response:
     """Function returning Response object"""
     if type(content) is str:
-        content = {}
+        return make_response({
+            'message': message,
+        }, code)
     return make_response({
         'message': message,
         'content': content,
