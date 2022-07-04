e70513ea9ec500ca16dcd0c6fce1026c6879d821
Łukasz Pitrus <pitrus@student.agh.edu.pl>
2022-07-04 11:42:08 +0200

    Update settings.py

--- yaptide/settings.py
+++ yaptide/settings.py
@@ -3,8 +3,6 @@
 Most configuration is set via environment variables.
 For local development, use a .env file to set
 environment variables.
-
-File taken from https://github.com/cookiecutter-flask/cookiecutter-flask/blob/master/%7B%7Bcookiecutter.app_name%7D%7D/%7B%7Bcookiecutter.app_name%7D%7D/settings.py
 """
 from environs import Env
 import os
@@ -16,8 +14,8 @@ env.read_env()
 ENV = env.str("FLASK_ENV", default="production")
 DEBUG = ENV == "development"
 SQLALCHEMY_DATABASE_URI = f'sqlite:////{file_dir}/data/main.db'
-# SECRET_KEY = env.str("SECRET_KEY")
-# SEND_FILE_MAX_AGE_DEFAULT = env.int("SEND_FILE_MAX_AGE_DEFAULT")
+# SECRET_KEY = env.str("SECRET_KEY") skipcq: PY-W0069
+# SEND_FILE_MAX_AGE_DEFAULT = env.int("SEND_FILE_MAX_AGE_DEFAULT") skipcq: PY-W0069
 BCRYPT_LOG_ROUNDS = env.int("BCRYPT_LOG_ROUNDS", default=13)
 DEBUG_TB_ENABLED = DEBUG
 DEBUG_TB_INTERCEPT_REDIRECTS = False
