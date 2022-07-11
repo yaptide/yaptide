b8b111a1636d3b0677a96feab2468d3b7511cb68
Łukasz Pitrus <pitrus@student.agh.edu.pl>
2022-07-11 11:34:48 +0200

    absolute db path and enums

--- yaptide/data/db_script.py
+++ yaptide/data/db_script.py
@@ -3,15 +3,31 @@ import os
 import json
 
 from pathlib import Path
+from enum import Enum
 
 import sqlalchemy as db
 from werkzeug.security import generate_password_hash
 
 
+class OperationTypes(Enum):
+    INSERT = "INSERT"
+    UPDATE = "UPDATE"
+    DELETE = "DELETE"
+
+
+class TableTypes(Enum):
+    USER = "User"
+
+
+class DataJsonFields(Enum):
+    LOGIN = "LOGIN"
+    PASSWORD = "PASSWORD"
+
+
 def select_all_users(con: db.engine.Connection):
     """Selects all users from db"""
     metadata = db.MetaData()
-    users = db.Table('User', metadata, autoload=True, autoload_with=engine)
+    users = db.Table(TableTypes.USER.value, metadata, autoload=True, autoload_with=engine)
     query = db.select([users])
     ResultProxy = con.execute(query)
     ResultSet = ResultProxy.fetchall()
@@ -21,48 +37,63 @@ def select_all_users(con: db.engine.Connection):
 def insert_user(con: db.engine.Connection, data: dict):
     """Inserts new user to db"""
     metadata = db.MetaData()
-    users = db.Table('User', metadata, autoload=True, autoload_with=engine)
+    users = db.Table(TableTypes.USER.value, metadata, autoload=True, autoload_with=engine)
     query = db.insert(users).values(
-        login_name=data["LOGIN"],
-        password_hash=generate_password_hash(data["PASSWORD"])
+        login_name=data[DataJsonFields.LOGIN.value],
+        password_hash=generate_password_hash(data[DataJsonFields.PASSWORD.value])
     )
     con.execute(query)
+    print(f'Successfully inserted user: {data[DataJsonFields.LOGIN.value]}')
 
 
 def update_user(con: db.engine.Connection, data: dict):
     """Updates user with provided login in db"""
     metadata = db.MetaData()
-    users = db.Table('User', metadata, autoload=True, autoload_with=engine)
+    users = db.Table(TableTypes.USER.value, metadata, autoload=True, autoload_with=engine)
     query = db.update(users).where(
-       users.c.login_name == data["LOGIN"]
+       users.c.login_name == data[DataJsonFields.LOGIN.value]
     ).values(
-        password_hash=generate_password_hash(data["PASSWORD"])
+        password_hash=generate_password_hash(data[DataJsonFields.PASSWORD.value])
     )
     con.execute(query)
+    print(f'Successfully updated user: {data[DataJsonFields.LOGIN.value]}')
 
 
 def delete_user(con: db.engine.Connection, data: dict):
     """Deletes user with provided login from db"""
     metadata = db.MetaData()
-    users = db.Table('User', metadata, autoload=True, autoload_with=engine)
+    users = db.Table(TableTypes.USER.value, metadata, autoload=True, autoload_with=engine)
     query = db.delete(users).where(
-       users.c.login_name == data["LOGIN"]
+       users.c.login_name == data[DataJsonFields.LOGIN.value]
     )
     con.execute(query)
+    print(f'Successfully deleted user: {data[DataJsonFields.LOGIN.value]}')
 
 
 if __name__ == "__main__":
-    input_json_path = Path(os.path.dirname(os.path.realpath(__file__)), 'script_input.json')
+
+    OPERATION = "OPERATION"
+    TABLE = "TABLE"
+    DATA = "DATA"
+    
+    file_dir = os.path.dirname(os.path.realpath(__file__))
+    input_json_path = Path(file_dir, 'script_input.json')
     with open(input_json_path) as json_file:
         json_data = json.load(json_file)
-    engine = db.create_engine('sqlite:///main.db')
+    engine = db.create_engine(f'sqlite:////{file_dir}/main.db')
     connection = engine.connect()
 
     for obj in json_data:
-        if obj["OPERATION"] == "INSERT" and obj["TABLE"] == "User":
-            insert_user(con=connection, data=obj["DATA"])
-        if obj["OPERATION"] == "UPDATE" and obj["TABLE"] == "User":
-            update_user(con=connection, data=obj["DATA"])
-        if obj["OPERATION"] == "DELETE" and obj["TABLE"] == "User":
-            delete_user(con=connection, data=obj["DATA"])
-        select_all_users(con=connection)
+        if OPERATION not in obj:
+            raise ValueError(f'No OPERATION field in provided JSON object: {obj}')
+        if TABLE not in obj:
+            raise ValueError(f'No TABLE field in provided JSON object: {obj}')
+        if DATA not in obj:
+            raise ValueError(f'No DATA field in provided JSON object: {obj}')
+        
+        if obj[OPERATION] == OperationTypes.INSERT.value and obj[TABLE] == TableTypes.USER.value:
+            insert_user(con=connection, data=obj[DATA])
+        if obj[OPERATION] == OperationTypes.UPDATE.value and obj[TABLE] == TableTypes.USER.value:
+            update_user(con=connection, data=obj[DATA])
+        if obj[OPERATION] == OperationTypes.DELETE.value and obj[TABLE] == TableTypes.USER.value:
+            delete_user(con=connection, data=obj[DATA])
