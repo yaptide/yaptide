0b59788b563a871abb4feb52d09359de004cc9f6
Łukasz Pitrus <pitrus@student.agh.edu.pl>
2022-10-23 18:25:27 +0200

    final version - before deepsource

--- yaptide_local_tester/yaptide_tester.py
+++ yaptide_local_tester/yaptide_tester.py
@@ -2,14 +2,15 @@ import argparse
 import json
 import math
 import os
+import sys
 import time
 import timeit
-
-from multiprocessing import Process
 
 from pathlib import Path
 
 import requests
+
+ROOT_DIR = os.path.dirname(os.path.realpath(__file__))
 
 
 class Endpoints:
@@ -29,41 +30,70 @@ class Endpoints:
         self.http_rimrock = f'http://{host}:{port}/plgrid/jobs'
         self.http_plgdata = f'http://{host}:{port}/plgrid/data'
 
+class YaptideTesterSession:
+    """"""
 
-AUTH_JSON = {
-    "login_name": "admin",
-    "password": "password",
-}
-EXAMPLE_DIR = os.path.dirname(os.path.realpath(__file__))
+    def __init__(self, login_endpoint: str, logout_endpoint: str):
+        """"""
+        self.session = requests.Session()
+        self.timer = timeit.default_timer()
+        self.login_interval = 500
+        self.login_endpoint = login_endpoint
+        self.logout_endpoint = logout_endpoint
+
+    def login(self, inital_login: bool = False):
+        """"""
+        if timeit.default_timer() - self.timer > self.login_interval or inital_login:
+            res: requests.Response = self.session.post(self.login_endpoint, json={"login_name": "admin", "password": "password"})
+            if res.status_code != 202:
+                res_json = res.json()
+                print(res_json)
+                sys.exit()
+
+    def logout(self):
+        """"""
+        self.session.delete(self.logout_endpoint)
+
+    def post(self, endpoint: str, json: dict) -> requests.Response:
+        """"""
+        self.login()
+        return self.session.post(endpoint, json=json)
+
+    def get(self, endpoint: str, params: dict = None) -> requests.Response:
+        """"""
+        self.login()
+        if params:
+            return self.session.get(endpoint, params=params)
+        else:
+            return self.session.get(endpoint)
 
 
 class YaptideTester:
     """"""
 
     def __init__(self, host: str = 'localhost', port: int = 5000):
-        self.session = requests.Session()
-        self.endpoints = Endpoints(host=host, port=port)
-        self.running = False
+        self.endpoints = Endpoints(host, port)
+        self.session = YaptideTesterSession(self.endpoints.http_auth_login, self.endpoints.http_auth_logout)
 
+    def run_all(self):
+        """"""
+        Path(ROOT_DIR, "output").mkdir(parents=True, exist_ok=True)
+        self.session.login(inital_login=True)
 
-    def run(self):
-        def login(session: requests.Session, login_endpoint: str):
-            while(True):
-                res: requests.Response = session.post(login_endpoint, json=AUTH_JSON)
-                print(res)
-                time.sleep(500)
-        login_thread = Process(target=login)
-        login_thread.start()
+        print("Running simulation on rimrock")
+        self.run_simulation_with_rimrock(True)
+        print("Running simulation on backend")
+        self.run_simulation_on_backend(True)
 
-        # TODO: actually run all here
+        self.session.logout()
 
-        login_thread.terminate()
-
+    @staticmethod
     def read_input_files() -> dict:
         """Read shieldhit input files from input_files folder"""
         input_files = {}
         for filename in ['geo.dat', 'detect.dat', 'beam.dat', 'mat.dat']:
-            file = Path(EXAMPLE_DIR, 'input_files', filename)
+            file = Path(ROOT_DIR, 'input_files', filename)
+            file.parent.mkdir(exist_ok=True, parents=True)
             with open(file, 'r') as reader:
                 input_files[filename] = reader.read()
         return input_files
@@ -71,19 +101,13 @@ class YaptideTester:
 
     def run_simulation_on_backend(self, do_monitor_job: bool = True):
         """Example client running simulation"""
-        example_json = Path(EXAMPLE_DIR, 'example.json')
+        example_json = Path(ROOT_DIR, 'example.json')
 
         with open(example_json) as json_file:
             json_to_send = json.load(json_file)
 
-        res: requests.Response = self.session.post(self.endpoints.http_auth_login, json=AUTH_JSON)
-
-        if res.status_code != 202:
-            print(res.json())
-            return
-
         if do_monitor_job:
-            self.run_simulation_with_json(json_to_send=json_to_send)
+            self.run_simulation_with_json(json_to_send)
 
             self.run_simulation_with_files()
         else:
@@ -92,8 +116,6 @@ class YaptideTester:
                 self.run_simulation_with_files(do_monitor_job=False)
                 time.sleep(5)
             self.check_backend_jobs(sim_n=sim_n, page_size=2)
-
-        self.session.delete(self.endpoints.http_auth_logout)
 
 
     def run_simulation_with_json(self, json_to_send, do_monitor_job: bool = True):
@@ -115,20 +137,20 @@ class YaptideTester:
                     # the request has succeeded, we can access its contents
                     if res.status_code == 200:
                         if data.get('result'):
-                            with open(Path(EXAMPLE_DIR, 'output', 'simulation_output.json'), 'w') as writer:
+                            with open(Path(ROOT_DIR, 'output', 'simulation_output.json'), 'w') as writer:
                                 data_to_write = str(data['result'])
                                 data_to_write = data_to_write.replace("'", "\"")
                                 writer.write(data_to_write)
                             return
                         if data.get('logfile'):
-                            with open(Path(EXAMPLE_DIR, 'output', 'error_full_output.json'), 'w') as writer:
+                            with open(Path(ROOT_DIR, 'output', 'error_full_output.json'), 'w') as writer:
                                 data_to_write = str(data)
                                 data_to_write = data_to_write.replace("'", "\"")
                                 writer.write(data_to_write)
-                            with open(Path(EXAMPLE_DIR, 'output', 'shieldlog.log'), 'w') as writer:
+                            with open(Path(ROOT_DIR, 'output', 'shieldlog.log'), 'w') as writer:
                                 writer.write(data['logfile'])
                             for key, value in data['input_files'].items():
-                                with open(Path(EXAMPLE_DIR, 'output', key), 'w') as writer:
+                                with open(Path(ROOT_DIR, 'output', key), 'w') as writer:
                                     writer.write(value)
                             return
                         if data.get('error'):
@@ -159,20 +181,20 @@ class YaptideTester:
                     # the request has succeeded, we can access its contents
                     if res.status_code == 200:
                         if data.get('result'):
-                            with open(Path(EXAMPLE_DIR, 'output', 'simulation_output.json'), 'w') as writer:
+                            with open(Path(ROOT_DIR, 'output', 'simulation_output.json'), 'w') as writer:
                                 data_to_write = str(data['result'])
                                 data_to_write = data_to_write.replace("'", "\"")
                                 writer.write(data_to_write)
                             return
                         if data.get('logfile'):
-                            with open(Path(EXAMPLE_DIR, 'output', 'error_full_output.json'), 'w') as writer:
+                            with open(Path(ROOT_DIR, 'output', 'error_full_output.json'), 'w') as writer:
                                 data_to_write = str(data)
                                 data_to_write = data_to_write.replace("'", "\"")
                                 writer.write(data_to_write)
-                            with open(Path(EXAMPLE_DIR, 'output', 'shieldlog.log'), 'w') as writer:
+                            with open(Path(ROOT_DIR, 'output', 'shieldlog.log'), 'w') as writer:
                                 writer.write(data['logfile'])
                             for key, value in data['input_files'].items():
-                                with open(Path(EXAMPLE_DIR, 'output', key), 'w') as writer:
+                                with open(Path(ROOT_DIR, 'output', key), 'w') as writer:
                                     writer.write(value)
                             return
                         if data.get('error'):
@@ -185,23 +207,12 @@ class YaptideTester:
 
     def check_backend_jobs(self, sim_n: int, page_size: int):
         """Example checking backend jobs with pagination"""
-        res: requests.Response = self.session.post(self.endpoints.http_auth_login, json=AUTH_JSON)
-        print(res.json())
-        if res.status_code != 202:
-            return
-        timer = timeit.default_timer()
         are_all_finished = False
         one_last_run = True
         while not are_all_finished and one_last_run:
             some_still_running = False
             for i in range(math.ceil(sim_n/page_size)):
                 time.sleep(5)
-                if timeit.default_timer() - timer > 500:
-                    res: requests.Response = self.session.post(self.endpoints.http_auth_login, json=AUTH_JSON)
-                    if res.status_code != 202:
-                        print(res.json())
-                        return
-                    timer = timeit.default_timer()
                 res: requests.Response = self.session.get(self.endpoints.http_list_sims, params={
                     "page_size": page_size,
                     "page_idx": i,
@@ -209,7 +220,6 @@ class YaptideTester:
                     "order_type": "descend",
                 })
                 res_json: dict = res.json()
-                print(res_json['simulations_count'])
                 for sim in res_json['simulations']:
                     print(sim)
                     task_id = sim['task_id']
@@ -217,25 +227,8 @@ class YaptideTester:
                     res_json: dict = res.json()
                     if "result" not in res_json and not some_still_running:
                         some_still_running = True
-                        print("Some still running")
             one_last_run = not are_all_finished
             are_all_finished = not some_still_running
-
-
-    # def read_grid_proxy_file(dir_path: str) -> str:
-    #     """Function reading grid_proxy file"""
-    #     grid_proxy_path = Path(dir_path, 'grid_proxy')
-    #     grid_proxy = ""
-    #     try:
-    #         with open(grid_proxy_path) as grid_proxy_file:
-    #             grid_proxy = grid_proxy_file.read()
-    #     except FileNotFoundError:
-    #         print("Generate grid_proxy file by adjusting following command:\n")
-    #         cmd = "read -s p && echo $p | ssh -l <plgusername> ares.cyfronet.pl "
-    #         cmd += r'"grid-proxy-init -q -pwstdin && cat /tmp/x509up_u\`id -u\`"'
-    #         cmd += f" > {grid_proxy_path} && unset p\n"
-    #         print(cmd)
-    #     return grid_proxy
 
 
     def run_simulation_with_rimrock(self, do_monitor_job: bool = True):
@@ -251,10 +244,8 @@ class YaptideTester:
         if job_id is not None:
             while do_monitor_job:
                 time.sleep(5)
-                res: requests.Response = self.session.get(self.endpoints.http_rimrock,
-                                                    params={"job_id": job_id})
+                res: requests.Response = self.session.get(self.endpoints.http_rimrock, params={"job_id": job_id})
                 res_json = res.json()
-                print(f'Rescode {res.status_code}')
                 print(res_json)
                 if res.status_code != 200:
                     return
@@ -272,10 +263,9 @@ class YaptideTester:
 
     def get_slurm_results(self, job_id: str):
         """Example function getting slurm results"""
-        res: requests.Response = self.session.get(self.endpoints.http_plgdata,
-                                            params={"job_id": job_id})
+        res: requests.Response = self.session.get(self.endpoints.http_plgdata, params={"job_id": job_id})
         res_json = res.json()
-        path = Path(EXAMPLE_DIR, 'output', f'{job_id.split(".")[0]}.json')
+        path = Path(ROOT_DIR, 'output', f'{job_id.split(".")[0]}.json')
         with open(path, 'w') as writer:
             data_to_write = str(res_json['result'])
             data_to_write = data_to_write.replace("'", "\"")
@@ -288,4 +278,4 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     tester = YaptideTester(port=args.port)
-    tester.run()
+    tester.run_all()
