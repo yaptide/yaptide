9af589c1648374d762033a725c7511109c7701d4
Łukasz Pitrus <pitrus@student.agh.edu.pl>
2022-10-24 09:39:20 +0200

    Update db_script.py

--- yaptide/data/db_script.py
+++ yaptide/data/db_script.py
@@ -33,9 +33,8 @@ class DataUserFields(Enum):
     GRID_PROXY_NAME = "GRID_PROXY_NAME"
 
 
-def select_all_simulations(con: db.engine.Connection, engine):
+def select_all_simulations(con: db.engine.Connection, metadata: db.MetaData, engine):
     """Selects all users from db"""
-    metadata = db.MetaData()
     simulations = db.Table(TableTypes.SIMULATION.value, metadata, autoload=True, autoload_with=engine)
     query = db.select([simulations])
     ResultProxy = con.execute(query)
@@ -43,9 +42,8 @@ def select_all_simulations(con: db.engine.Connection, engine):
     print(ResultSet)
 
 
-def select_all_users(con: db.engine.Connection, engine):
+def select_all_users(con: db.engine.Connection, metadata: db.MetaData, engine):
     """Selects all users from db"""
-    metadata = db.MetaData()
     users = db.Table(TableTypes.USER.value, metadata, autoload=True, autoload_with=engine)
     query = db.select([users])
     ResultProxy = con.execute(query)
@@ -53,9 +51,8 @@ def select_all_users(con: db.engine.Connection, engine):
     print(ResultSet)
 
 
-def insert_user(con: db.engine.Connection, engine, data: dict):
+def insert_user(con: db.engine.Connection, metadata: db.MetaData, engine, data: dict):
     """Inserts new user to db"""
-    metadata = db.MetaData()
     users = db.Table(TableTypes.USER.value, metadata, autoload=True, autoload_with=engine)
     query = db.insert(users).values(
         login_name=data[DataUserFields.LOGIN.value],
@@ -68,9 +65,8 @@ def insert_user(con: db.engine.Connection, engine, data: dict):
         print(f'Inserting user: {data[DataUserFields.LOGIN.value]} failed, probably already exists')
 
 
-def update_user(con: db.engine.Connection, engine, data: dict):
+def update_user(con: db.engine.Connection, metadata: db.MetaData, engine, data: dict):
     """Updates user with provided login in db"""
-    metadata = db.MetaData()
     users = db.Table(TableTypes.USER.value, metadata, autoload=True, autoload_with=engine)
     if DataUserFields.LOGIN.value not in data:
         print(f'{DataUserFields.LOGIN.value} not provided in UPDATE function')
@@ -92,9 +88,8 @@ def update_user(con: db.engine.Connection, engine, data: dict):
     print(f'Successfully updated user: {data[DataUserFields.LOGIN.value]}')
 
 
-def delete_user(con: db.engine.Connection, engine, data: dict):
+def delete_user(con: db.engine.Connection, metadata: db.MetaData, engine, data: dict):
     """Deletes user with provided login from db"""
-    metadata = db.MetaData()
     users = db.Table(TableTypes.USER.value, metadata, autoload=True, autoload_with=engine)
     query = db.delete(users).where(users.c.login_name == data[DataUserFields.LOGIN.value])
     con.execute(query)
@@ -113,6 +108,7 @@ if __name__ == "__main__":
         json_data = json.load(json_file)
     db_engine = db.create_engine(f'sqlite:////{file_dir}/main.db')
     connection = db_engine.connect()
+    db_metadata = db.MetaData()
 
     for obj in json_data:
         if OPERATION not in obj:
@@ -123,13 +119,13 @@ if __name__ == "__main__":
             raise ValueError(f'No DATA field in provided JSON object: {obj}')
 
         if obj[OPERATION] == OperationTypes.INSERT.value and obj[TABLE] == TableTypes.USER.value:
-            insert_user(con=connection, engine=db_engine, data=obj[DATA])
+            insert_user(con=connection, metadata=db_metadata, engine=db_engine, data=obj[DATA])
         if obj[OPERATION] == OperationTypes.UPDATE.value and obj[TABLE] == TableTypes.USER.value:
-            update_user(con=connection, engine=db_engine, data=obj[DATA])
+            update_user(con=connection, metadata=db_metadata, engine=db_engine, data=obj[DATA])
         if obj[OPERATION] == OperationTypes.DELETE.value and obj[TABLE] == TableTypes.USER.value:
-            delete_user(con=connection, engine=db_engine, data=obj[DATA])
+            delete_user(con=connection, metadata=db_metadata, engine=db_engine, data=obj[DATA])
         if obj[OPERATION] == OperationTypes.SELECT.value:
             if obj[TABLE] == TableTypes.USER.value:
-                select_all_users(con=connection, engine=db_engine)
+                select_all_users(con=connection, metadata=db_metadata, engine=db_engine)
             else:
-                select_all_simulations(con=connection, engine=db_engine)
+                select_all_simulations(con=connection, metadata=db_metadata, engine=db_engine)
