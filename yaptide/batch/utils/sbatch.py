89ce93a71a1153430731c8621b3519552e1bbf1a
Łukasz Pitrus <pitrus@student.agh.edu.pl>
2023-04-17 23:21:11 +0200

    fix

--- yaptide/batch/utils/sbatch.py
+++ yaptide/batch/utils/sbatch.py
@@ -1,14 +1,14 @@
 import re
 
 
-def sanitize_string(target_str: str, allowed_chars: str = r'[\w\\-.,=/:]+') -> str:
+def sanitize_string(target_str: str, allowed_chars: str = r'[\w\-.,=/:]+') -> str:
     """Function clearing unaccepted signs"""
     return re.sub(f'[^\\s{allowed_chars}]', '', target_str)
 
 
 def extract_sbatch_header(json_data: dict, target_key: str) -> str:
     """Function extracting header for slurm script"""
-    return (sanitize_string(json_data["batch_options"][target_key], r'[\w\\-.,=/:#]+')
+    return (sanitize_string(json_data["batch_options"][target_key], r'[\w\-.,=/:#]+')
             if "batch_options" in json_data
             and target_key in json_data["batch_options"]
             else "")
