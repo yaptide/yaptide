72aba3365bd76456512005169b07aef59e458a1d
Leszek Grzanka <leszek.grzanka@gmail.com>
2023-04-30 00:05:15 +0200

    celery tests

--- tests/test_celery.py
+++ tests/test_celery.py
@@ -1,28 +1,15 @@
 import pytest
+from celery.contrib.testing.tasks import ping
 
-# tasks.py
-from celery import Celery, shared_task
-from yaptide.celery.tasks import cancel_simulation
+from yaptide.celery.tasks import cancel_simulation, dupa
 
-# Define the Celery app
-celery_app = Celery(
-    'tasks',
-    broker='memory://',
-    backend='rpc://',
-)
-
-# Register the Celery app with pytest
 @pytest.fixture(scope='session')
-def app():
-    return celery_app
+def celery_app(request):
+    from yaptide.celery.worker import celery_app as app
+    return app
 
-@shared_task
-def mul(x, y):
-    return x * y
 
-# def test_mul(celery_app, celery_worker):    
-#     assert mul.delay(4, 4).get(timeout=10) == 16
-
-#@pytest.mark.celery(result_backend='rpc', broker_url='memory://')
 def test_cancel_simulation(celery_app, celery_worker):
-    assert cancel_simulation.delay("test").get(timeout=1) == True
+    job = cancel_simulation.delay(job_id="test")
+    result: dict = job.wait()
+    assert result == False
