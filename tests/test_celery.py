3f29f00e36db2b0776666528cf748d2b09a186e0
Leszek Grzanka <leszek.grzanka@gmail.com>
2023-04-29 22:28:57 +0200

    testing

--- tests/test_celery.py
+++ tests/test_celery.py
@@ -1,42 +1,28 @@
-# test_app.py
-
 import pytest
-from redis import Redis
-#from yaptide.application import create_app
-#from yaptide.celery.worker import celery_app as celery
 
-@pytest.fixture(scope="module")
-def redis_server():
-    """Start a Redis server."""
-    redis_server = Redis(port=16379)
-    yield redis_server
-    redis_server.close()
+# tasks.py
+from celery import Celery, shared_task
+from yaptide.celery.tasks import cancel_simulation
 
-def test_redis_connection(redis_server):
-    """Test the Redis connection."""
-    redis_server.ping()
+# Define the Celery app
+celery_app = Celery(
+    'tasks',
+    broker='memory://',
+    backend='rpc://',
+)
 
-# @pytest.fixture(scope='module')
-# def test_client():
-#     app = create_app('testing')
-#     with app.test_client() as testing_client:
-#         with app.app_context():
-#             yield testing_client
+# Register the Celery app with pytest
+@pytest.fixture(scope='session')
+def app():
+    return celery_app
 
+@shared_task
+def mul(x, y):
+    return x * y
 
-# @pytest.fixture(scope='module')
-# def celery_app(request):
-#     celery.conf.update(CELERY_ALWAYS_EAGER=True)
-#     return celery
+# def test_mul(celery_app, celery_worker):    
+#     assert mul.delay(4, 4).get(timeout=10) == 16
 
-
-# def test_celery_task(celery_app):
-#     result = celery_app.send_task('tasks.add', args=[1, 2])
-#     assert result.get() == 3
-
-
-# # def test_redis_connection(test_client):
-
-# #     response = test_client.get('/redis')
-# #     assert response.status_code == 200
-# #     assert response.data == b'Connection to Redis server succeeded!'
+#@pytest.mark.celery(result_backend='rpc', broker_url='memory://')
+def test_cancel_simulation(celery_app, celery_worker):
+    assert cancel_simulation.delay("test").get(timeout=1) == True
