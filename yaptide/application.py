c3e484a19a86fa05e37b6e7f84230083e8cd534c
kacper1409 <kacper1409@gmail.com>
2023-05-12 16:58:52 +0200

    added flask_swagger_ui to requirements & set up swagger in application.py

--- yaptide/application.py
+++ yaptide/application.py
@@ -4,11 +4,26 @@ from yaptide.routes.main_routes import initialize_routes
 from yaptide.persistence.database import db
 from yaptide.persistence import models
 from flask_cors import CORS
+from flask_swagger_ui import get_swaggerui_blueprint
 
 
 def create_app(config_object="yaptide.settings"):
     """Function starting Flask Server"""
     app = Flask(__name__.split('.')[0])
+
+    SWAGGER_URL = '/api/docs'
+    API_URL = 'http://petstore.swagger.io/v2/swagger.json'
+
+    swaggerui_blueprint = get_swaggerui_blueprint(
+        SWAGGER_URL,  # Swagger UI static files will be mapped to '{SWAGGER_URL}/dist/'
+        API_URL,
+        config={  # Swagger UI config overrides
+            'app_name': "yaptide"
+        }
+    )
+
+    app.register_blueprint(swaggerui_blueprint)
+
     app.config.from_object(config_object)
     db.init_app(app)
     CORS(app, supports_credentials=True)
