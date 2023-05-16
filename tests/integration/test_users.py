b232f35680fb96241e84b8e81a4f5020eb90a92f
Leszek Grzanka <leszek.grzanka@gmail.com>
2023-05-16 11:15:55 +0200

    cleaning

--- tests/integration/test_users.py
+++ tests/integration/test_users.py
@@ -12,86 +12,61 @@ _Username_2 = "not_existing_user"
 _Password = "test_password"
 
 
-@pytest.fixture(scope='module')
-def app() -> Generator[Flask, None, None]:  # skipcq: PY-D0003
-    _app = create_app()
-    with _app.app_context():
-        db.create_all()
-    yield _app
-
-    with _app.app_context():
-        db.drop_all()
-
-
-@pytest.fixture(scope='module')
-def client(app):  # skipcq: PY-D0003
-    _client = app.test_client()
-    yield _client
-
-
-def test_register(client):
+def test_register(client_fixture):
     """Test if user can register"""
-    resp = client.put("/auth/register", data=json.dumps(dict(
-        username=_Username,
-        password=_Password)),
-        content_type='application/json')
+    resp = client_fixture.put("/auth/register",
+                              data=json.dumps(dict(username=_Username, password=_Password)),
+                              content_type='application/json')
 
     assert resp.status_code == 201  # skipcq: BAN-B101
 
 
-def test_register_existing(client):
+def test_register_existing(client_fixture):
     """Test if user can register"""
-    client.put("/auth/register", data=json.dumps(dict(
-        username=_Username,
-        password=_Password)),
-        content_type='application/json')
-    resp = client.put("/auth/register", data=json.dumps(dict(
-        username=_Username,
-        password=_Password)),
-        content_type='application/json')
+    client_fixture.put("/auth/register",
+                       data=json.dumps(dict(username=_Username, password=_Password)),
+                       content_type='application/json')
+    resp = client_fixture.put("/auth/register",
+                              data=json.dumps(dict(username=_Username, password=_Password)),
+                              content_type='application/json')
 
     assert resp.status_code == 403  # skipcq: BAN-B101
 
 
-def test_log_in(client):
+def test_log_in(client_fixture):
     """Test if user can log in"""
-    client.put("/auth/register", data=json.dumps(dict(
-        username=_Username,
-        password=_Password)),
-        content_type='application/json')
-    resp = client.post("/auth/login", data=json.dumps(dict(
-        username=_Username,
-        password=_Password)),
-        content_type='application/json')
+    client_fixture.put("/auth/register",
+                       data=json.dumps(dict(username=_Username, password=_Password)),
+                       content_type='application/json')
+    resp = client_fixture.post("/auth/login",
+                               data=json.dumps(dict(username=_Username, password=_Password)),
+                               content_type='application/json')
 
     assert resp.status_code == 202  # skipcq: BAN-B101
     assert resp.headers['Set-Cookie']  # skipcq: BAN-B101
 
 
-def test_log_in_not_existing(client):
+def test_log_in_not_existing(client_fixture):
     """Test if user can log in"""
-    resp = client.post("/auth/login", data=json.dumps(dict(
-        username=_Username_2,
-        password=_Password)),
-        content_type='application/json')
+    resp = client_fixture.post("/auth/login",
+                               data=json.dumps(dict(username=_Username_2, password=_Password)),
+                               content_type='application/json')
 
     assert resp.status_code == 401  # skipcq: BAN-B101
 
 
-def test_user_status(client):
+def test_user_status(client_fixture):
     """Test checking user's status"""
-    resp = client.put("/auth/register", data=json.dumps(dict(
-        username=_Username,
-        password=_Password)),
-        content_type='application/json')
-    resp = client.post("/auth/login", data=json.dumps(dict(
-        username=_Username,
-        password=_Password)),
-        content_type='application/json')
+    resp = client_fixture.put("/auth/register",
+                              data=json.dumps(dict(username=_Username, password=_Password)),
+                              content_type='application/json')
+    resp = client_fixture.post("/auth/login",
+                               data=json.dumps(dict(username=_Username, password=_Password)),
+                               content_type='application/json')
 
     sleep(10)
 
-    resp = client.get("/auth/status")
+    resp = client_fixture.get("/auth/status")
 
     data = json.loads(resp.data.decode())
 
@@ -99,34 +74,32 @@ def test_user_status(client):
     assert resp.status_code == 200  # skipcq: BAN-B101
 
 
-def test_user_status_unauthorized(client):
+def test_user_status_unauthorized(client_fixture):
     """Test checking user's status"""
-    resp = client.get("/auth/status")
+    resp = client_fixture.get("/auth/status")
     logging.info(resp.data)
     assert resp.status_code == 401  # skipcq: BAN-B101
 
 
-def test_user_status_after_logout(client):
+def test_user_status_after_logout(client_fixture):
     """Test checking user's status"""
-    client.put("/auth/register", data=json.dumps(dict(
-        username=_Username,
-        password=_Password)),
-        content_type='application/json')
-    client.post("/auth/login", data=json.dumps(dict(
-        username=_Username,
-        password=_Password)),
-        content_type='application/json')
+    client_fixture.put("/auth/register",
+                       data=json.dumps(dict(username=_Username, password=_Password)),
+                       content_type='application/json')
+    client_fixture.post("/auth/login",
+                        data=json.dumps(dict(username=_Username, password=_Password)),
+                        content_type='application/json')
 
     sleep(10)
 
-    resp = client.get("/auth/status")
+    resp = client_fixture.get("/auth/status")
 
     assert resp.status_code == 200  # skipcq: BAN-B101
 
-    resp = client.delete("/auth/logout")
+    resp = client_fixture.delete("/auth/logout")
 
     assert resp.status_code == 200  # skipcq: BAN-B101
 
-    resp = client.get("/auth/status")
+    resp = client_fixture.get("/auth/status")
 
     assert resp.status_code == 401  # skipcq: BAN-B101
