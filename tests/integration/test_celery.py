c797e7a446f7498167e2613af407ffb28183b48b
Leszek Grzanka <leszek.grzanka@gmail.com>
2023-05-21 22:53:46 +0200

    fix

--- tests/integration/test_celery.py
+++ tests/integration/test_celery.py
@@ -20,7 +20,8 @@ from yaptide.utils.sim_utils import files_dict_with_adjusted_primaries
 
 def test_run_simulation(celery_app, 
                         celery_worker, 
-                        payload_editor_dict_data : dict, 
+                        payload_editor_dict_data : dict,
+                        live_server, 
                         client,
                         add_directory_to_path,
                         shieldhit_demo_binary):
