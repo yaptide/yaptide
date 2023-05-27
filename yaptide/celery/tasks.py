92565b6be8f41a24341a2529a1bc908b6f1059e8
Leszek Grzanka <leszek.grzanka@gmail.com>
2023-05-27 22:27:36 +0200

    new method

--- yaptide/celery/tasks.py
+++ yaptide/celery/tasks.py
@@ -4,14 +4,38 @@ import tempfile
 from datetime import datetime
 from multiprocessing import Process
 from pathlib import Path
+from celery.signals import worker_ready
 
 from pymchelper.executor.options import SimulationSettings
 from pymchelper.executor.runner import Runner as SHRunner
+from yaptide.admin.simulators import SimulatorType, install_simulator
 
 from yaptide.celery.utils.utils import read_file, send_simulation_results, send_simulation_logfiles
 from yaptide.celery.worker import celery_app
 from yaptide.utils.sim_utils import (check_and_convert_payload_to_files_dict, pymchelper_output_to_json,
                                      simulation_logfiles, write_simulation_input_files)
+
+
+@worker_ready.connect
+def on_worker_ready(**kwargs):
+    '''This function will be called when celery worker is ready to accept tasks'''
+    logging.info("on_worker_ready signal received")
+    
+    # ask celery to install simulator on the worker and wait for it to finish
+    job = install_simulators.delay()
+    # we need to do some hackery here to make blocking calls work
+    # method described in https://stackoverflow.com/questions/33280456 doesn't work.
+    try:
+        job.wait()
+    except RuntimeError as e:
+        logging.info("the only way for blocking calls to work is to catch RuntimeError", e)
+        raise e
+
+
+@celery_app.task()
+def install_simulators() -> bool:
+    result = install_simulator(SimulatorType.shieldhit)
+    return result
 
 
 @celery_app.task(bind=True)
