1312e7c55977d98c4d94ec9d06966f8207416f71
Łukasz Pitrus <pitrus@student.agh.edu.pl>
2023-07-31 14:24:35 +0200

    repaired login

--- yaptide/routes/auth_routes.py
+++ yaptide/routes/auth_routes.py
@@ -1,15 +1,14 @@
 from flask import request
 from flask_restful import Resource
-
-from marshmallow import Schema, ValidationError
-from marshmallow import fields
+from marshmallow import Schema, ValidationError, fields
 
 from yaptide.persistence.database import db
 from yaptide.persistence.models import YaptideUserModel
-
-from yaptide.routes.utils.tokens import encode_auth_token
 from yaptide.routes.utils.decorators import requires_auth
-from yaptide.routes.utils.response_templates import yaptide_response, error_internal_response, error_validation_response
+from yaptide.routes.utils.response_templates import (error_internal_response,
+                                                     error_validation_response,
+                                                     yaptide_response)
+from yaptide.routes.utils.tokens import encode_auth_token
 
 
 class AuthRegister(Resource):
@@ -30,16 +29,13 @@ class AuthRegister(Resource):
             return error_validation_response()
 
         try:
-            user = db.session.query(YaptideUserModel).filter_by(
-                username=json_data.get('username')).first()
+            user = db.session.query(YaptideUserModel).filter_by(username=json_data['username']).first()
         except Exception:  # skipcq: PYL-W0703
             return error_internal_response()
 
         if not user:
             try:
-                user = YaptideUserModel(
-                    username=json_data.get('username')
-                )
+                user = YaptideUserModel(username=json_data.get('username'))
                 user.set_password(json_data.get('password'))
 
                 db.session.add(user)
@@ -56,26 +52,25 @@ class AuthRegister(Resource):
 class AuthLogIn(Resource):
     """Class responsible for user log in"""
 
-    class APIParametersSchema(Schema):
-        """Class specifies API parameters"""
-
-        username = fields.String()
-        password = fields.String()
-
     @staticmethod
     def post():
         """Method returning status of logging in (and token if it was successful)"""
-        try:
-            json_data: dict = AuthLogIn.APIParametersSchema().load(request.get_json(force=True))
-        except ValidationError:
-            return error_validation_response()
+        payload_dict: dict = request.get_json(force=True)
+        if not payload_dict:
+            return yaptide_response(message="No JSON in body", code=400)
+
+        required_keys = {"username", "password"}
+
+        if required_keys != required_keys.intersection(set(payload_dict.keys())):
+            diff = required_keys.difference(set(payload_dict.keys()))
+            return yaptide_response(message=f"Missing keys in JSON payload: {diff}", code=400)
 
         try:
-            user = db.session.query(YaptideUserModel).filter_by(username=json_data.get('username')).first()
+            user: YaptideUserModel = db.session.query(YaptideUserModel).filter_by(username=payload_dict['username']).first()
             if not user:
                 return yaptide_response(message='Invalid login or password', code=401)
 
-            if not user.check_password(password=json_data.get('password')):
+            if not user.check_password(password=payload_dict['password']):
                 return yaptide_response(message='Invalid login or password', code=401)
 
             access_token, access_exp = encode_auth_token(user_id=user.id, is_refresh=False)
