37dc87a344e47de5a22b4676b91c67341d45e74f
Łukasz Pitrus <pitrus@student.agh.edu.pl>
2023-07-31 14:52:38 +0200

    dip sauce

--- yaptide/routes/common_sim_routes.py
+++ yaptide/routes/common_sim_routes.py
@@ -7,233 +7,233 @@ from marshmallow import Schema, fields
 from yaptide.batch.batch_methods import get_job_results
 from yaptide.persistence.database import db
 from yaptide.persistence.models import (
-	ClusterModel, EstimatorModel, InputModel, LogfilesModel, PageModel, SimulationModel, UserBaseModel)
+    ClusterModel, EstimatorModel, InputModel, LogfilesModel, PageModel, SimulationModel, UserBaseModel)
 from yaptide.routes.utils.decorators import requires_auth
 from yaptide.routes.utils.response_templates import error_validation_response, yaptide_response
 from yaptide.routes.utils.utils import check_if_job_is_owned_and_exist
 
 
 class SimulationResults(Resource):
-	"""Class responsible for managing results"""
+    """Class responsible for managing results"""
 
-	@staticmethod
-	def post():
-		"""
-		Method for saving results
-		Used by the jobs at the end of simulation
-		Structure required by this method to work properly:
-		{
-			"simulation_id": <int>,
-			"update_key": <string>,
-			"estimators": <dict>
-		}
-		"""
-		payload_dict: dict = request.get_json(force=True)
-		if {"simulation_id", "update_key", "estimators"} != set(payload_dict.keys()):
-			return yaptide_response(message="Incomplete JSON data", code=400)
+    @staticmethod
+    def post():
+        """
+        Method for saving results
+        Used by the jobs at the end of simulation
+        Structure required by this method to work properly:
+        {
+            "simulation_id": <int>,
+            "update_key": <string>,
+            "estimators": <dict>
+        }
+        """
+        payload_dict: dict = request.get_json(force=True)
+        if {"simulation_id", "update_key", "estimators"} != set(payload_dict.keys()):
+            return yaptide_response(message="Incomplete JSON data", code=400)
 
-		sim_id = payload_dict["simulation_id"]
-		simulation: SimulationModel = db.session.query(SimulationModel).filter_by(id=sim_id).first()
+        sim_id = payload_dict["simulation_id"]
+        simulation: SimulationModel = db.session.query(SimulationModel).filter_by(id=sim_id).first()
 
-		if not simulation:
-			return yaptide_response(message="Simulation does not exist", code=400)
+        if not simulation:
+            return yaptide_response(message="Simulation does not exist", code=400)
 
-		if not simulation.check_update_key(payload_dict["update_key"]):
-			return yaptide_response(message="Invalid update key", code=400)
+        if not simulation.check_update_key(payload_dict["update_key"]):
+            return yaptide_response(message="Invalid update key", code=400)
 
-		for estimator_dict in payload_dict["estimators"]:
-			# We forsee the possibility of the estimator being created earlier as element of partial results
-			estimator: EstimatorModel = db.session.query(EstimatorModel).filter_by(
-				simulation_id=sim_id, name=estimator_dict["name"]).first()
+        for estimator_dict in payload_dict["estimators"]:
+            # We forsee the possibility of the estimator being created earlier as element of partial results
+            estimator: EstimatorModel = db.session.query(EstimatorModel).filter_by(
+                simulation_id=sim_id, name=estimator_dict["name"]).first()
 
-			if not estimator:
-				estimator = EstimatorModel(name=estimator_dict["name"], simulation_id=simulation.id)
-				estimator.data = estimator_dict["metadata"]
-				db.session.add(estimator)
-				db.session.commit()
+            if not estimator:
+                estimator = EstimatorModel(name=estimator_dict["name"], simulation_id=simulation.id)
+                estimator.data = estimator_dict["metadata"]
+                db.session.add(estimator)
+                db.session.commit()
 
-			for page_dict in estimator_dict["pages"]:
-				page: PageModel = db.session.query(PageModel).filter_by(
-					estimator_id=estimator.id, page_number=int(page_dict["metadata"]["page_number"])).first()
+            for page_dict in estimator_dict["pages"]:
+                page: PageModel = db.session.query(PageModel).filter_by(
+                    estimator_id=estimator.id, page_number=int(page_dict["metadata"]["page_number"])).first()
 
-				page_existed = bool(page)
-				if not page_existed:
-					# create new page
-					page = PageModel(page_number=int(page_dict["metadata"]["page_number"]), estimator_id=estimator.id)
-				# we always update the data
-				page.data = page_dict
-				if not page_existed:
-					# if page was created, we add it to the session
-					db.session.add(page)
-			db.session.commit()
+                page_existed = bool(page)
+                if not page_existed:
+                    # create new page
+                    page = PageModel(page_number=int(page_dict["metadata"]["page_number"]), estimator_id=estimator.id)
+                # we always update the data
+                page.data = page_dict
+                if not page_existed:
+                    # if page was created, we add it to the session
+                    db.session.add(page)
+            db.session.commit()
 
-		return yaptide_response(message="Results saved", code=202)
+        return yaptide_response(message="Results saved", code=202)
 
-	class APIParametersSchema(Schema):
-		"""Class specifies API parameters"""
+    class APIParametersSchema(Schema):
+        """Class specifies API parameters"""
 
-		job_id = fields.String()
+        job_id = fields.String()
 
-	@staticmethod
-	@requires_auth()
-	def get(user: UserBaseModel):
-		"""Method returning job status and results"""
-		schema = SimulationResults.APIParametersSchema()
-		errors: dict[str, list[str]] = schema.validate(request.args)
-		if errors:
-			return yaptide_response(message="Wrong parameters", code=400, content=errors)
-		param_dict: dict = schema.load(request.args)
+    @staticmethod
+    @requires_auth()
+    def get(user: UserBaseModel):
+        """Method returning job status and results"""
+        schema = SimulationResults.APIParametersSchema()
+        errors: dict[str, list[str]] = schema.validate(request.args)
+        if errors:
+            return yaptide_response(message="Wrong parameters", code=400, content=errors)
+        param_dict: dict = schema.load(request.args)
 
-		job_id = param_dict['job_id']
-		is_owned, error_message, res_code = check_if_job_is_owned_and_exist(job_id=job_id, user=user)
-		if not is_owned:
-			return yaptide_response(message=error_message, code=res_code)
+        job_id = param_dict['job_id']
+        is_owned, error_message, res_code = check_if_job_is_owned_and_exist(job_id=job_id, user=user)
+        if not is_owned:
+            return yaptide_response(message=error_message, code=res_code)
 
-		simulation: SimulationModel = db.session.query(SimulationModel).filter_by(job_id=job_id).first()
+        simulation: SimulationModel = db.session.query(SimulationModel).filter_by(job_id=job_id).first()
 
-		estimators: list[EstimatorModel] = db.session.query(EstimatorModel).filter_by(simulation_id=simulation.id).all()
-		if len(estimators) == 0:
-			if simulation.platform == "DIRECT":  # also CODE TO REMOVE
-				return yaptide_response(message="Results are unavailable", code=404)
-			# Code below is for backward compatibility with old method of saving results
-			# later on we are going to remove it because it's functionality will be covered
-			# by the post method
-			# BEGIN CODE TO REMOVE
-			try:
-				_, _, _, cluster_name = job_id.split(":")
-			except ValueError:
-				return error_validation_response(content={"message": "Job ID is incorrect"})
+        estimators: list[EstimatorModel] = db.session.query(EstimatorModel).filter_by(simulation_id=simulation.id).all()
+        if len(estimators) == 0:
+            if simulation.platform == "DIRECT":  # also CODE TO REMOVE
+                return yaptide_response(message="Results are unavailable", code=404)
+            # Code below is for backward compatibility with old method of saving results
+            # later on we are going to remove it because it's functionality will be covered
+            # by the post method
+            # BEGIN CODE TO REMOVE
+            try:
+                _, _, _, cluster_name = job_id.split(":")
+            except ValueError:
+                return error_validation_response(content={"message": "Job ID is incorrect"})
 
-			cluster: ClusterModel = db.session.query(ClusterModel).\
-				filter_by(cluster_name=cluster_name).first()
+            cluster: ClusterModel = db.session.query(ClusterModel).\
+                filter_by(cluster_name=cluster_name).first()
 
-			result: dict = get_job_results(concat_job_id=job_id, user=user, cluster=cluster)
-			if "estimators" not in result:
-				logging.debug("Results for job %s are unavailable", job_id)
-				return yaptide_response(message="Results are unavailable", code=404, content=result)
+            result: dict = get_job_results(concat_job_id=job_id, user=user, cluster=cluster)
+            if "estimators" not in result:
+                logging.debug("Results for job %s are unavailable", job_id)
+                return yaptide_response(message="Results are unavailable", code=404, content=result)
 
-			for estimator_dict in result["estimators"]:
-				estimator = EstimatorModel(name=estimator_dict["name"], simulation_id=simulation.id)
-				estimator.data = estimator_dict["metadata"]
-				db.session.add(estimator)
-				db.session.commit()
-				for page_dict in estimator_dict["pages"]:
-					page = PageModel(estimator_id=estimator.id,
-									 page_number=int(page_dict["metadata"]["page_number"]))
-					page.data = page_dict
-					db.session.add(page)
-				db.session.commit()
-			estimators: list[EstimatorModel] = db.session.query(EstimatorModel).\
-				filter_by(simulation_id=simulation.id).all()
-			# END CODE TO REMOVE
+            for estimator_dict in result["estimators"]:
+                estimator = EstimatorModel(name=estimator_dict["name"], simulation_id=simulation.id)
+                estimator.data = estimator_dict["metadata"]
+                db.session.add(estimator)
+                db.session.commit()
+                for page_dict in estimator_dict["pages"]:
+                    page = PageModel(estimator_id=estimator.id,
+                                     page_number=int(page_dict["metadata"]["page_number"]))
+                    page.data = page_dict
+                    db.session.add(page)
+                db.session.commit()
+            estimators: list[EstimatorModel] = db.session.query(EstimatorModel).\
+                filter_by(simulation_id=simulation.id).all()
+            # END CODE TO REMOVE
 
-		logging.debug("Returning results from database")
-		result_estimators = []
-		for estimator in estimators:
-			pages: list[PageModel] = db.session.query(PageModel).filter_by(estimator_id=estimator.id).all()
-			estimator_dict = {
-				"metadata": estimator.data,
-				"name": estimator.name,
-				"pages": [page.data for page in pages]
-			}
-			result_estimators.append(estimator_dict)
-		return yaptide_response(message=f"Results for job: {job_id}", code=200,
-								content={"estimators": result_estimators})
+        logging.debug("Returning results from database")
+        result_estimators = []
+        for estimator in estimators:
+            pages: list[PageModel] = db.session.query(PageModel).filter_by(estimator_id=estimator.id).all()
+            estimator_dict = {
+                "metadata": estimator.data,
+                "name": estimator.name,
+                "pages": [page.data for page in pages]
+            }
+            result_estimators.append(estimator_dict)
+        return yaptide_response(message=f"Results for job: {job_id}", code=200,
+                                content={"estimators": result_estimators})
 
 
 class SimulationInputs(Resource):
-	"""Class responsible for returning simulation input"""
+    """Class responsible for returning simulation input"""
 
-	class APIParametersSchema(Schema):
-		"""Class specifies API parameters"""
+    class APIParametersSchema(Schema):
+        """Class specifies API parameters"""
 
-		job_id = fields.String()
+        job_id = fields.String()
 
-	@staticmethod
-	@requires_auth()
-	def get(user: UserBaseModel):
-		"""Method returning simulation input"""
-		schema = SimulationInputs.APIParametersSchema()
-		errors: dict[str, list[str]] = schema.validate(request.args)
-		if errors:
-			return yaptide_response(message="Wrong parameters", code=400, content=errors)
-		param_dict: dict = schema.load(request.args)
-		job_id = param_dict['job_id']
+    @staticmethod
+    @requires_auth()
+    def get(user: UserBaseModel):
+        """Method returning simulation input"""
+        schema = SimulationInputs.APIParametersSchema()
+        errors: dict[str, list[str]] = schema.validate(request.args)
+        if errors:
+            return yaptide_response(message="Wrong parameters", code=400, content=errors)
+        param_dict: dict = schema.load(request.args)
+        job_id = param_dict['job_id']
 
-		is_owned, error_message, res_code = check_if_job_is_owned_and_exist(job_id=job_id, user=user)
-		if not is_owned:
-			return yaptide_response(message=error_message, code=res_code)
+        is_owned, error_message, res_code = check_if_job_is_owned_and_exist(job_id=job_id, user=user)
+        if not is_owned:
+            return yaptide_response(message=error_message, code=res_code)
 
-		simulation: SimulationModel = db.session.query(SimulationModel).filter_by(job_id=job_id).first()
+        simulation: SimulationModel = db.session.query(SimulationModel).filter_by(job_id=job_id).first()
 
-		input_model: InputModel = db.session.query(InputModel).filter_by(simulation_id=simulation.id).first()
-		if not input_model:
-			return yaptide_response(message="Input of simulation is unavailable", code=404)
+        input_model: InputModel = db.session.query(InputModel).filter_by(simulation_id=simulation.id).first()
+        if not input_model:
+            return yaptide_response(message="Input of simulation is unavailable", code=404)
 
-		return yaptide_response(message="Input of simulation", code=200, content={"input": input_model.data})
+        return yaptide_response(message="Input of simulation", code=200, content={"input": input_model.data})
 
 
 class SimulationLogfiles(Resource):
-	"""Class responsible for managing logfiles"""
+    """Class responsible for managing logfiles"""
 
-	@staticmethod
-	def post():
-		"""
-		Method for saving logfiles
-		Used by the jobs when the simulation fails
-		Structure required by this method to work properly:
-		{
-			"simulation_id": <int>,
-			"update_key": <string>,
-			"logfiles": <dict>
-		}
-		"""
-		payload_dict: dict = request.get_json(force=True)
-		if {"simulation_id", "update_key", "logfiles"} != set(payload_dict.keys()):
-			return yaptide_response(message="Incomplete JSON data", code=400)
+    @staticmethod
+    def post():
+        """
+        Method for saving logfiles
+        Used by the jobs when the simulation fails
+        Structure required by this method to work properly:
+        {
+            "simulation_id": <int>,
+            "update_key": <string>,
+            "logfiles": <dict>
+        }
+        """
+        payload_dict: dict = request.get_json(force=True)
+        if {"simulation_id", "update_key", "logfiles"} != set(payload_dict.keys()):
+            return yaptide_response(message="Incomplete JSON data", code=400)
 
-		sim_id = payload_dict["simulation_id"]
-		simulation: SimulationModel = db.session.query(SimulationModel).filter_by(id=sim_id).first()
+        sim_id = payload_dict["simulation_id"]
+        simulation: SimulationModel = db.session.query(SimulationModel).filter_by(id=sim_id).first()
 
-		if not simulation:
-			return yaptide_response(message="Simulation does not exist", code=400)
+        if not simulation:
+            return yaptide_response(message="Simulation does not exist", code=400)
 
-		if not simulation.check_update_key(payload_dict["update_key"]):
-			return yaptide_response(message="Invalid update key", code=400)
+        if not simulation.check_update_key(payload_dict["update_key"]):
+            return yaptide_response(message="Invalid update key", code=400)
 
-		logfiles = LogfilesModel(simulation_id=simulation.id)
-		logfiles.data = payload_dict["logfiles"]
-		db.session.add(logfiles)
-		db.session.commit()
+        logfiles = LogfilesModel(simulation_id=simulation.id)
+        logfiles.data = payload_dict["logfiles"]
+        db.session.add(logfiles)
+        db.session.commit()
 
-		return yaptide_response(message="Log files saved", code=202)
+        return yaptide_response(message="Log files saved", code=202)
 
-	class APIParametersSchema(Schema):
-		"""Class specifies API parameters"""
+    class APIParametersSchema(Schema):
+        """Class specifies API parameters"""
 
-		job_id = fields.String()
+        job_id = fields.String()
 
-	@staticmethod
-	@requires_auth()
-	def get(user: UserBaseModel):
-		"""Method returning job status and results"""
-		schema = SimulationResults.APIParametersSchema()
-		errors: dict[str, list[str]] = schema.validate(request.args)
-		if errors:
-			return yaptide_response(message="Wrong parameters", code=400, content=errors)
-		param_dict: dict = schema.load(request.args)
+    @staticmethod
+    @requires_auth()
+    def get(user: UserBaseModel):
+        """Method returning job status and results"""
+        schema = SimulationResults.APIParametersSchema()
+        errors: dict[str, list[str]] = schema.validate(request.args)
+        if errors:
+            return yaptide_response(message="Wrong parameters", code=400, content=errors)
+        param_dict: dict = schema.load(request.args)
 
-		job_id = param_dict['job_id']
-		is_owned, error_message, res_code = check_if_job_is_owned_and_exist(job_id=job_id, user=user)
-		if not is_owned:
-			return yaptide_response(message=error_message, code=res_code)
+        job_id = param_dict['job_id']
+        is_owned, error_message, res_code = check_if_job_is_owned_and_exist(job_id=job_id, user=user)
+        if not is_owned:
+            return yaptide_response(message=error_message, code=res_code)
 
-		simulation: SimulationModel = db.session.query(SimulationModel).filter_by(job_id=job_id).first()
+        simulation: SimulationModel = db.session.query(SimulationModel).filter_by(job_id=job_id).first()
 
-		logfile: LogfilesModel = db.session.query(LogfilesModel).filter_by(simulation_id=simulation.id).first()
-		if not logfile:
-			return yaptide_response(message="Logfiles are unavailable", code=404)
+        logfile: LogfilesModel = db.session.query(LogfilesModel).filter_by(simulation_id=simulation.id).first()
+        if not logfile:
+            return yaptide_response(message="Logfiles are unavailable", code=404)
 
-		logging.debug("Returning logfiles from database")
+        logging.debug("Returning logfiles from database")
 
-		return yaptide_response(message="Logfiles", code=200, content={"logfiles": logfile.data})
+        return yaptide_response(message="Logfiles", code=200, content={"logfiles": logfile.data})
