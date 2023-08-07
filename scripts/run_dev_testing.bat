35954405cd2660c90aa777c5f066daddfff9f53a
Łukasz Pitrus <pitrus@student.agh.edu.pl>
2023-08-07 14:19:37 +0200

    notification and results work with yaptide_tester

--- scripts/run_dev_testing.bat
+++ scripts/run_dev_testing.bat
@@ -1,31 +1,30 @@
-@REM 1 argument stands for plgrid username
-@REM 2 argument stands for yaptide being killed after tests or not
-@REM 3 argument stands for yaptide tester mode
-call scripts\run_yaptide.bat %1
+@REM 1 argument stands for yaptide being killed after tests or not
+@REM 2 argument stands for yaptide tester mode
+call scripts\run_yaptide.bat
 
 @echo off
-if "%3" == "jd" (
+if "%2" == "jd" (
     py .\yaptide_tester\yaptide_tester.py --do_monitor --test_jsons --run_direct
-) else if "%3" == "fd" (
+) else if "%2" == "fd" (
     py .\yaptide_tester\yaptide_tester.py --do_monitor --test_files --run_direct
-) else if "%3" == "jb" (
+) else if "%2" == "jb" (
     py .\yaptide_tester\yaptide_tester.py --do_monitor --test_jsons --run_batch
-) else if "%3" == "fb" (
+) else if "%2" == "fb" (
     py .\yaptide_tester\yaptide_tester.py --do_monitor --test_files --run_batch
-) else if "%3" == "jfd" (
+) else if "%2" == "jfd" (
     py .\yaptide_tester\yaptide_tester.py --do_monitor --test_jsons --test_files --run_direct
-) else if "%3" == "jfb" (
+) else if "%2" == "jfb" (
     py .\yaptide_tester\yaptide_tester.py --do_monitor --test_jsons --test_files --run_batch
-) else if "%3" == "jdb" (
+) else if "%2" == "jdb" (
     py .\yaptide_tester\yaptide_tester.py --do_monitor --test_jsons --run_direct --run_batch
-) else if "%3" == "fdb" (
+) else if "%2" == "fdb" (
     py .\yaptide_tester\yaptide_tester.py --do_monitor --test_files --run_direct --run_batch
 ) else (
     py .\yaptide_tester\yaptide_tester.py --do_monitor --all
 )
 @echo on
 
-if "%2" == "no_kill" (
+if "%1" == "no_kill" (
     echo "Leaving yaptide backend running"
 ) else (
     call scripts\kill_yaptide.bat
--- scripts/run_yaptide.bat
+++ scripts/run_yaptide.bat
@@ -2,7 +2,6 @@ FOR %%A IN ("%~dp0.") DO SET folder=%%~dpA
 
 docker volume prune --force
 docker-compose up -d --build
-docker cp id_ed25519 yaptide_flask:/usr/local/app
 docker exec -w /usr/local/app/ yaptide_flask python3 yaptide/admin/db_manage.py add-user admin --password password
 docker exec -w /usr/local/app/ yaptide_flask python3 yaptide/admin/db_manage.py add-cluster ares.cyfronet.pl
 docker exec -w /usr/local/app/ yaptide_flask python3 yaptide/admin/db_manage.py list-users
