be54eded662e81cd972a60930f7b7d5af6a41863
Mateusz Wronka <matwro2001@gmail.com>
2023-10-07 18:25:54 +0200

    adjust dockerfile and script for worker

--- run_worker.sh
+++ run_worker.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 ./yaptide/admin/simulators.py install --name shieldhit
+./yaptide/admin/simulators.py install --name topas
 
 celery --app yaptide.celery.worker worker -E --loglevel=info -P eventlet --hostname yaptide-worker
