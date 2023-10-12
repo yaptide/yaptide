9477764ea6236e5260c828c4ecde96c82494ffdc
Mateusz Wronka <matwro2001@gmail.com>
2023-10-12 18:22:32 +0200

    optional topas installation

--- run_worker.sh
+++ run_worker.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 
 ./yaptide/admin/simulators.py install --name shieldhit
-./yaptide/admin/simulators.py install --name topas
+
+if [ -z "$S3_TOPAS_BUCKET" ] || [ -z "$S3_TOPAS_KEY" ] || [ -z "$S3_TOPAS_VERSION" ] || [ -z "$S3_GEANT_BUCKET" ]; then
+        echo "One or more environment variables required by TOPAS are not set, skipping TOPAS installation"
+    else
+        apt-get update
+
+        apt-get install -y --no-install-recommends \
+        libexpat1-dev \
+        libgl1-mesa-dev \
+        libglu1-mesa-dev \
+        libxt-dev \
+        xorg-dev \
+        build-essential \
+        libharfbuzz-dev \
+        gfortran \
+
+        rm -rf /var/lib/apt/lists/*
+
+        /yaptide/admin/simulators.py install --name topas
+fi
 
 celery --app yaptide.celery.worker worker -E --loglevel=info -P eventlet --hostname yaptide-worker
