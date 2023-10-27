1bb4e349c185ef1b5dca0a63d809fcc9262e72a2
Leszek Grzanka <leszek.grzanka@gmail.com>
2023-10-27 23:10:18 +0200

    fix install

--- run_worker.sh
+++ run_worker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-./yaptide/admin/simulators.py install --name shieldhit --path /simulators/shieldhit12a/bin
+./yaptide/admin/simulators.py download-shieldhit --dir /simulators/shieldhit12a/bin --decrypt
 
 if [ -z "$S3_TOPAS_BUCKET" ] || [ -z "$S3_TOPAS_KEY" ] || [ -z "$S3_TOPAS_VERSION" ] || [ -z "$S3_GEANT4_BUCKET" ]; then
         echo "One or more environment variables required by TOPAS are not set, skipping TOPAS installation"
