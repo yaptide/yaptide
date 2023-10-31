5658f7616adcecb0608290c025fea405c2a80147
Leszek Grzanka <leszek.grzanka@gmail.com>
2023-10-31 12:30:16 +0100

    check if we have access to yaptide (#689)
    * check if we have access to yaptide
    
    * fix tests

--- yaptide/routes/keycloak_routes.py
+++ yaptide/routes/keycloak_routes.py
@@ -8,12 +8,9 @@ import json
 import jwt
 import requests
 
-from yaptide.persistence.db_methods import (add_object_to_db,
-                                            fetch_keycloak_user_by_username,
-                                            make_commit_to_db)
+from yaptide.persistence.db_methods import (add_object_to_db, fetch_keycloak_user_by_username, make_commit_to_db)
 from yaptide.persistence.models import KeycloakUserModel
-from yaptide.routes.utils.response_templates import (error_internal_response,
-                                                     yaptide_response)
+from yaptide.routes.utils.response_templates import (error_internal_response, yaptide_response)
 from yaptide.routes.utils.tokens import encode_auth_token
 from werkzeug.exceptions import Forbidden, Unauthorized
 
@@ -34,11 +31,10 @@ def check_user_based_on_keycloak_token(token: str, username: str) -> bool:
     try:
         # first lets try to decode token without verifying signature
         unverified_encoded_token = jwt.decode(token, options={"verify_signature": False})
-        # very crude way to check by username comparison
-        # we will later update this place by checking if use has access to our yaptite platform
-        if username != unverified_encoded_token["preferred_username"]:
-            logging.error("Username mismatch")
-            raise Forbidden(description="Username mismatch")
+        # check if token gives access to our service
+        if unverified_encoded_token.get("plgridAccessServices") != "PLG_YAPTIDE_ACCESS":
+            logging.error("User %s has no access to our service", username)
+            raise Forbidden(description=f"User {username} has no access to our service")
 
         # ask keycloak for public keys
         res = requests.get(keycloak_full_url)
@@ -101,9 +97,7 @@ class AuthKeycloak(Resource):
 
         # ask cert auth service for cert and private key
         session = requests.Session()
-        res: requests.Response = session.get(cert_auth_url, headers={
-            'Authorization': keycloak_token
-        })
+        res: requests.Response = session.get(cert_auth_url, headers={'Authorization': keycloak_token})
         res_json: dict = res.json()
         logging.debug("auth cert service response code: %d", res.status_code)
         logging.debug("auth cert service response mesg: %s", res_json)
@@ -112,9 +106,7 @@ class AuthKeycloak(Resource):
         user = fetch_keycloak_user_by_username(username=username)
         if not user:
             # user not existing, adding user together with cert and private key
-            user = KeycloakUserModel(username=username,
-                                     cert=res_json["cert"],
-                                     private_key=res_json["private"])
+            user = KeycloakUserModel(username=username, cert=res_json["cert"], private_key=res_json["private"])
 
             add_object_to_db(user)
         else:
@@ -125,16 +117,13 @@ class AuthKeycloak(Resource):
 
         try:
             # prepare our own tokens
-            access_token, access_exp = encode_auth_token(user_id=user.id,
-                                                         is_keycloak=True)
+            access_token, access_exp = encode_auth_token(user_id=user.id, is_keycloak=True)
 
-            resp = yaptide_response(
-                message='Successfully logged in',
-                code=202,
-                content={
-                    'access_exp': int(access_exp.timestamp()*1000),
-                }
-            )
+            resp = yaptide_response(message='Successfully logged in',
+                                    code=202,
+                                    content={
+                                        'access_exp': int(access_exp.timestamp() * 1000),
+                                    })
             resp.set_cookie('access_token', access_token, httponly=True, samesite='Lax', expires=access_exp)
             return resp
         except Exception:  # skipcq: PYL-W0703
