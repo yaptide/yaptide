a92619000e4fbba2a87141dcbe068d7fba362faf
Dominik Hendzel <128628266+hendzeld@users.noreply.github.com>
2023-11-06 00:55:06 +0100

    Add reader with logs instead output progress

--- yaptide/celery/utils/progress/fluka_monitor.py
+++ yaptide/celery/utils/progress/fluka_monitor.py
@@ -1,3 +1,5 @@
+import logging
+from pathlib import Path
 import re
 from typing import Optional, Tuple
 
@@ -12,6 +14,8 @@ S_OK_OUT_FIN = re.compile(r"^ \* ======(?:( )*?)End of FLUKA [\w\-.]* run (?:( )
 # "              1          999999999999          999999999999         2.8598309E-04         1.0000000E+30                 0       "
 S_OK_OUT_PROGRESS_REMAINING_LINE_EXAMPLE = \
     "     980000                 20000                 20000             1.8515483E-04         1.0000000E+30             44182       "
+
+logger = logging.getLogger(__name__)
 
 
 def parse_progress_remaining_line(line: str) -> Optional[Tuple[int, int]]:
@@ -32,3 +36,37 @@ def parse_progress_remaining_line(line: str) -> Optional[Tuple[int, int]]:
     except ValueError:
         return None
     return (int(list[0]), int(list[1]))
+
+
+def read_fluka_out_file(file_path: Path, varbose: bool = False):
+    """Function reading the fluka output file and returning the progress and remaining values.
+
+    Returns:
+        Tuple[int, int]: tuple with two integers representing the progress and remaining.
+        If the line cannot be parsed None is returned.
+    """
+    in_progress = False
+    with open(file_path) as file:
+        for line in file:
+            if re.match(S_OK_OUT_START, line):
+                logger.debug("Found start of the simulation")
+                continue
+            if re.match(S_OK_OUT_IN_PROGRESS, line):
+                in_progress = True
+                if varbose:
+                    logger.debug("Found progress line")
+                continue
+            if re.match(S_OK_OUT_COLLECTED, line):
+                in_progress = False
+                if varbose:
+                    logger.debug("Found end of simulation calculation line")
+                continue
+            if re.match(S_OK_OUT_FIN, line):
+                logger.debug("Found end of the simulation")
+                break
+            if in_progress:
+                res = parse_progress_remaining_line(line)
+                if res:
+                    progress, remainder = res
+                    logger.debug("Found progress remaining line with progress: %s, remaining: %s", progress, remainder)
+                    continue
