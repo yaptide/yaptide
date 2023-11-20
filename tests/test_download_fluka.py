82e6582b68fe443b01ff7be0a6e3c30e8cd3d646
Kacper Barczyk <62296330+kacper1409@users.noreply.github.com>
2023-11-20 19:23:09 +0100

    configure yaptide dockerfiles to use fluka binaries from s3 (#692)
    * #394 added template run_worker.sh
    
    * #394 added fluka download test body
    
    * #394
    
    * #473 fixed download_fluka_from_s3 function call
    
    * #473
    
    * #473
    
    * #473 fix env
    
    * Download and unpack fluka from S3
    
    * Test downloading fluka from S3
    
    * Fix deepsource issues
    
    * Add fluka related variables to test run
    
    * Fix copying of fluka files
    
    * Fix lib path
    
    * Update worker entrypoint script
    
    * Extract function
    
    ---------
    
    Co-authored-by: Dominik Hendzel <128628266+hendzeld@users.noreply.github.com>
    Co-authored-by: Dominik Hendzel <dhendzel248@gmail.com>

---
+++ tests/test_download_fluka.py
@@ -0,0 +1,33 @@
+import os
+import sys
+import pytest
+import logging
+from pathlib import Path
+from tests.conftest import check_if_environment_variables_set
+from yaptide.admin.simulator_storage import download_fluka_from_s3
+
+
+@pytest.mark.skipif(sys.platform == "win32", reason="FLUKA does not work on Windows.")
+def test_if_fluka_downloaded(tmpdir):
+    """Check if FLUKA is downloaded and can be executed"""
+    variables = ['S3_ENDPOINT', 'S3_ACCESS_KEY', 'S3_SECRET_KEY', 'S3_FLUKA_BUCKET', 'S3_FLUKA_KEY']
+    if check_if_environment_variables_set(variables):
+        assert download_fluka_from_s3(
+            download_dir=tmpdir,
+            endpoint=os.getenv("S3_ENDPOINT"),
+            access_key=os.getenv("S3_ACCESS_KEY"),
+            secret_key=os.getenv("S3_SECRET_KEY"),
+            bucket=os.getenv("S3_FLUKA_BUCKET"),
+            password=os.getenv("S3_ENCRYPTION_PASSWORD"),
+            salt=os.getenv("S3_ENCRYPTION_SALT"),
+            key=os.getenv("S3_FLUKA_KEY"),
+        )
+
+        expected_rfluka_path = Path(tmpdir / "fluka" / "bin" / "rfluka")
+        expected_lib_path = Path(tmpdir / "fluka" / "lib")
+        assert expected_rfluka_path.exists(), "Expected FLUKA path does not exist."
+        assert expected_rfluka_path.stat().st_size > 0, "Expected FLUKA path is empty."
+        assert expected_lib_path.exists(), "Expected FLUKA lib does not exist."
+    else:
+        logging.warning("S3 environment variables are not set, skipping test.")
+        pytest.skip("S3 environment variables are not set.")
