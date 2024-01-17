54a19453597cc85a7e6fa3b9daf84219cd5e77e3
Leszek Grzanka <grzanka@agh.edu.pl>
2024-01-17 21:34:18 +0000

    no empty msgs processing and batch processing implementation inp

--- yaptide/redis_consumers/redis_consumer_base.py
+++ yaptide/redis_consumers/redis_consumer_base.py
@@ -5,14 +5,15 @@ from flask import Flask
 from yaptide.redis.redis import redis_client
 
 class RedisConsumerBase(Thread):
-    def __init__(self, app: Flask, consumer_name: str, queue_name: str):
+    def __init__(self, app: Flask, consumer_name: str, queue_name: str, batch_size: int):
         Thread.__init__(self)
         self.consumer_name = consumer_name
         self.queue_name = queue_name
         self.app = app
+        self.batch_size = batch_size
 
     @abstractmethod
-    def handle_message(self, message) -> None:
+    def handle_message(self, messages) -> None:
         pass
     def log_message_info(self, message: str) -> None:
         logging.info(f"Consumer {self.consumer_name}: {message}")
@@ -21,7 +22,13 @@ class RedisConsumerBase(Thread):
     def run(self) -> None:
         self.log_message_info("starts working...")
         while True:
-            message = redis_client.blpop([self.queue_name])[1]
+            message = redis_client.lpop(self.queue_name, count = batch_size)
+            if(len(message) != 0):
+                execute_handler
+            else:
+                time.sleep(1)
+
+    def execute_handler(): 
             self.log_message_info(f"Received message: {message}")
             self.handle_message(message)
             self.log_message_info(f"Processed message successfully")
--- yaptide/redis_consumers/task_progress_consumer.py
+++ yaptide/redis_consumers/task_progress_consumer.py
@@ -5,36 +5,37 @@ import json
 
 class TaskProgressConsumerThread(RedisConsumerBase):
     def __init__(self, app: Flask):
-        RedisConsumerBase.__init__(self, app, "TaskProgressConsumer", "task_updates")
+        RedisConsumerBase.__init__(self, app, "TaskProgressConsumer", "task_updates", 50)
         
-    def handle_message(self, message: str) -> None:
-        self.update_task_progress(message)
+    def handle_message(self, messages: list[str]) -> None:
+        self.update_task_progress(messages)
 
-    def update_task_progress(self, message: str) -> None:
-        payload_dict: dict = json.loads(message)
+    def update_task_progress(self, messages: list[str]) -> None:
+        payload_dicts: list[dict] = [json.loads(message) for message in messages]
         required_keys = {"simulation_id", "task_id", "update_key", "update_dict"}
-        if required_keys != set(payload_dict.keys()):
-            diff = required_keys.difference(set(payload_dict.keys()))
-            self.log_message_error(f"Missing keys in JSON payload: {diff}")
-            return
-
-        sim_id: int = payload_dict["simulation_id"]
-        with self.app.app_context():
-            simulation = fetch_simulation_by_sim_id(sim_id=sim_id)
-
-            if not simulation:
-                self.log_message_error(f"Simulation {sim_id} does not exist")
+        for payload_dict in payload_dicts:
+            if required_keys != set(payload_dict.keys()):
+                diff = required_keys.difference(set(payload_dict.keys()))
+                self.log_message_error(f"Missing keys in JSON payload: {diff}")
                 return
 
-            if not simulation.check_update_key(payload_dict["update_key"]):
-                self.log_message_error("Invalid update key")
-                return
-            
-            task = fetch_task_by_sim_id_and_task_id(sim_id=simulation.id, task_id=payload_dict["task_id"])
+            sim_id: int = payload_dict["simulation_id"]
+            with self.app.app_context():
+                simulation = fetch_simulation_by_sim_id(sim_id=sim_id)
 
-            if not task:
-                self.log_message_error(f"Task {payload_dict['task_id']} does not exist")
-                return
+                if not simulation:
+                    self.log_message_error(f"Simulation {sim_id} does not exist")
+                    return
 
-            update_task_state(task=task, update_dict=payload_dict["update_dict"])
-            self.log_message_info("Successfully updated task")
+                if not simulation.check_update_key(payload_dict["update_key"]):
+                    self.log_message_error("Invalid update key")
+                    return
+                
+                task = fetch_task_by_sim_id_and_task_id(sim_id=simulation.id, task_id=payload_dict["task_id"])
+
+                if not task:
+                    self.log_message_error(f"Task {payload_dict['task_id']} does not exist")
+                    return
+
+                update_task_state(task=task, update_dict=payload_dict["update_dict"])
+                self.log_message_info("Successfully updated task")
