1e87b655acef7a2ff02271f048c0161600a63459
Bartłomiej Szymański <115034357+SzymanskiBartlomiej@users.noreply.github.com>
2024-04-09 21:34:24 +0200

    add warning if  repository was cloned without submodules (#875)
    * move logic to yaptide/admin. Copying .gitmodules to docker containers
    
    * Review fixes
    
    * add windows and linux support
    
    * Moved check_submodules() at the start of create_app()
    
    * add submodule name, convert code to pathlib

--- yaptide/celery/worker.py
+++ yaptide/celery/worker.py
@@ -1,3 +1,6 @@
 from celery import Celery
+from yaptide.admin import git_submodules
 
+# celery.tasks is importing the converter module which is cloned as submodule.
+git_submodules.check_submodules()
 celery_app = Celery("celery", include=['yaptide.celery.tasks'])
