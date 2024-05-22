cbe995932965143b7b4958653b59834726e90c65
Michał Godek <mchlgdk@gmail.com>
2024-05-22 17:33:59 +0200

    doesn't work on wsl

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -10,7 +10,6 @@ import pytest
 from yaptide.admin.simulator_storage import download_shieldhit_from_s3_or_from_website
 
 from yaptide.application import create_app
-from yaptide.persistence.database import db
 
 
 @pytest.fixture(scope='session')
@@ -224,12 +223,70 @@ def app(tmp_path):
 
     logging.info("Creating Flask app for testing, time = %s", datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
 
+    from yaptide.persistence.database import db
     app = create_app()
     with app.app_context():
         db.drop_all()
         db.create_all()
 
+    # from flask_apscheduler import APScheduler
+    # scheduler = APScheduler()
+    
+
+    
+    # scheduler.init_app(app)
+   
+
+    # @scheduler.task('interval', id='save_tasks_progres_from_redis', minutes=2)
+    # def save_tasks_progres_from_redis():
+    #     """
+    #     Save tasks updates that are enqueued to redis queue "task_updates".
+    #     Main goal of this job is to process batched updates in database
+    #     and reduce load of POST /tasks endpoint.
+    #     """
+    #     from datetime import datetime
+    #     import json
+    #     logging.info("SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS")
+    #     from yaptide.persistence.db_methods import fetch_simulation_by_sim_id, fetch_task_by_sim_id_and_task_id, update_tasks_states
+    #     from yaptide.redis.redis import get_redis_client
+    #     redis_client = get_redis_client()
+    #     # Pop 1000 or less messages from left end of queue.
+    #     messages = redis_client.lpop('task_updates', count = 1000)
+    #     # Queue can be empty if there are no tasks to update
+    #     if messages == None or len(messages) == 0:
+    #         logging.info('No tasks received from redis')
+    #         return
+        
+    #     start = datetime.now()
+        
+    #     # Deserialize all received task updates messages
+    #     payload_dicts: list[dict] = [json.loads(message) for message in messages]
+    #     tasks_to_update = []
+    #     # Required to process data from oldest to newest - to prevent overriding new state by old for one task
+    #     payload_dicts.reverse()
+    #     with app.app_context():
+    #         for payload_dict in payload_dicts:
+    #             sim_id = payload_dict["simulation_id"]
+    #             task_id = payload_dict["task_id"]
+    #             task = fetch_task_by_sim_id_and_task_id(sim_id = sim_id, task_id = task_id)
+    #             if not task:
+    #                 logging.warning(f"Simulation {sim_id}: task {payload_dict['task_id']} does not exist")
+    #                 continue
+    #             tasks_to_update.append((task, payload_dict["update_dict"]))
+    #         # Batch update of all accepted tasks
+    #         update_tasks_states(tasks_to_update)
+    #     finish = datetime.now()
+    #     elapsed = (finish - start).total_seconds()
+    #     logging.info(f"Tasks processed: {len(messages)}, tasks updated: {len(tasks_to_update)}, time elapsed: {elapsed}s")
+    #     logging.info("Successfully updated tasks")
+    
+    
+    # scheduler._scheduler.start()
+
     yield app
+
+    # scheduler.remove_job('save_tasks_progres_from_redis')
+    # scheduler.shutdown()
 
     # clean up the database
     with app.app_context():
@@ -272,3 +329,64 @@ def live_server_win():
         yield server
     finally:
         server.terminate()
+
+# @pytest.fixture(scope="function")
+# def run_shceduler_task(app):
+#     from flask_apscheduler import APScheduler
+#     scheduler = APScheduler()
+    
+
+#     from datetime import datetime
+#     import json
+#     import logging
+#     from yaptide.persistence.db_methods import fetch_simulation_by_sim_id, fetch_task_by_sim_id_and_task_id, update_tasks_states
+#     from yaptide.redis.redis import get_redis_client
+
+
+#     scheduler.init_app(app)
+#     scheduler.start()
+#     with app.app_context():
+#         scheduler.add_job(
+#             id="save_tasks_progres_from_redis",
+#             func = save_tasks_progres_from_redis_job,
+#             trigger="interval",
+#             seconds=2,
+#             args=(scheduler.app,))
+        
+#     @scheduler.task('interval', id='save_tasks_progres_from_redis', minutes=2)
+#     def save_tasks_progres_from_redis_job(app):
+#         """
+#         Save tasks updates that are enqueued to redis queue "task_updates".
+#         Main goal of this job is to process batched updates in database
+#         and reduce load of POST /tasks endpoint.
+#         """
+#         redis_client = get_redis_client()
+#         # Pop 1000 or less messages from left end of queue.
+#         messages = redis_client.lpop('task_updates', count = 1000)
+#         # Queue can be empty if there are no tasks to update
+#         if messages == None or len(messages) == 0:
+#             logging.info('No tasks received from redis')
+#             return
+        
+#         start = datetime.now()
+        
+#         # Deserialize all received task updates messages
+#         payload_dicts: list[dict] = [json.loads(message) for message in messages]
+#         tasks_to_update = []
+#         # Required to process data from oldest to newest - to prevent overriding new state by old for one task
+#         payload_dicts.reverse()
+#         with app.app_context():
+#             for payload_dict in payload_dicts:
+#                 sim_id = payload_dict["simulation_id"]
+#                 task_id = payload_dict["task_id"]
+#                 task = fetch_task_by_sim_id_and_task_id(sim_id = sim_id, task_id = task_id)
+#                 if not task:
+#                     logging.warning(f"Simulation {sim_id}: task {payload_dict['task_id']} does not exist")
+#                     continue
+#                 tasks_to_update.append((task, payload_dict["update_dict"]))
+#             # Batch update of all accepted tasks
+#             update_tasks_states(tasks_to_update)
+#         finish = datetime.now()
+#         elapsed = (finish - start).total_seconds()
+#         logging.info(f"Tasks processed: {len(messages)}, tasks updated: {len(tasks_to_update)}, time elapsed: {elapsed}s")
+#         logging.info("Successfully updated tasks")
--- yaptide/application.py
+++ yaptide/application.py
@@ -8,6 +8,7 @@ from yaptide.persistence.database import db
 from yaptide.routes.main_routes import initialize_routes
 from yaptide.scheduler.scheduler import run_scheduler
 from yaptide.admin import git_submodules
+import logging
 
 def create_app():
     """Function starting Flask Server"""
@@ -60,7 +61,8 @@ def create_app():
 
     app.logger.info(f"Initializing Flask to use SQLAlchemy ORM @ {app.config['SQLALCHEMY_DATABASE_URI']}")
     db.init_app(app)
-    run_scheduler(app)
+    if not app.config['TESTING']:
+        run_scheduler(app)
 
     # Find a better solution (maybe with Flask-Migrate) to handle migration of data from past versions
     with app.app_context():
@@ -70,6 +72,57 @@ def create_app():
 
     api = Api(app)
     initialize_routes(api)
+
+    from flask_apscheduler import APScheduler
+    scheduler = APScheduler()
+    scheduler.api_enabled = True
+    scheduler.init_app(app)
+
+    @scheduler.task('interval', id='save_tasks_progres_from_redis_job', minutes=2)
+    def save_tasks_progres_from_redis_job():
+        """
+        Save tasks updates that are enqueued to redis queue "task_updates".
+        Main goal of this job is to process batched updates in database
+        and reduce load of POST /tasks endpoint.
+        """
+        from datetime import datetime
+        import json
+        logging.info("SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS")
+        from yaptide.persistence.db_methods import fetch_simulation_by_sim_id, fetch_task_by_sim_id_and_task_id, update_tasks_states
+        from yaptide.redis.redis import get_redis_client
+        redis_client = get_redis_client()
+        # Pop 1000 or less messages from left end of queue.
+        messages = redis_client.lpop('task_updates', count = 1000)
+        # Queue can be empty if there are no tasks to update
+        if messages == None or len(messages) == 0:
+            logging.info('No tasks received from redis')
+            return
+        
+        start = datetime.now()
+        
+        # Deserialize all received task updates messages
+        payload_dicts: list[dict] = [json.loads(message) for message in messages]
+        tasks_to_update = []
+        # Required to process data from oldest to newest - to prevent overriding new state by old for one task
+        payload_dicts.reverse()
+        with app.app_context():
+            for payload_dict in payload_dicts:
+                sim_id = payload_dict["simulation_id"]
+                task_id = payload_dict["task_id"]
+                task = fetch_task_by_sim_id_and_task_id(sim_id = sim_id, task_id = task_id)
+                if not task:
+                    logging.warning(f"Simulation {sim_id}: task {payload_dict['task_id']} does not exist")
+                    continue
+                tasks_to_update.append((task, payload_dict["update_dict"]))
+            # Batch update of all accepted tasks
+            update_tasks_states(tasks_to_update)
+        finish = datetime.now()
+        elapsed = (finish - start).total_seconds()
+        logging.info(f"Tasks processed: {len(messages)}, tasks updated: {len(tasks_to_update)}, time elapsed: {elapsed}s")
+        logging.info("Successfully updated tasks")
+    
+    
+    scheduler._scheduler.start()
 
     return app
 
