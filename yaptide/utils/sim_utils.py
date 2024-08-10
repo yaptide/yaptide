698cb5cd6fa25bb78a5bf26fd01c78d0bc6d189e
Bartłomiej Szymański <barteksz601@protonmail.com>
2024-08-10 15:08:19 +0200

    Use converter as a poetry dependency instead of submodule

--- yaptide/utils/sim_utils.py
+++ yaptide/utils/sim_utils.py
@@ -9,10 +9,7 @@ from pathlib import Path
 from pymchelper.estimator import Estimator
 from pymchelper.writers.json import JsonWriter
 from pymchelper.flair.Input import Card
-
-# dirty hack needed to properly handle relative imports in the converter submodule
-sys.path.append("yaptide/converter")
-from ..converter.converter.api import (get_parser_from_str, run_parser)  # skipcq: FLK-E402
+from converter.api import (get_parser_from_str, run_parser)
 
 NSTAT_MATCH = r"NSTAT\s*\d*\s*\d*"
 
