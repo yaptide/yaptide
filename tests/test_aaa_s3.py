0c16810551e310f2df2081d13d0d990e6323e02f
Leszek Grzanka <leszek.grzanka@gmail.com>
2024-11-09 17:49:05 +0100

    fix

--- tests/test_aaa_s3.py
+++ tests/test_aaa_s3.py
@@ -3,4 +3,4 @@ import socket
 def test_ip():
     hostname = 's3p.cloud.cyfronet.pl'
     ip = socket.gethostbyname(hostname)
-    assert ip == '149.156.237.14' or ip == '149.156.237.13'
+    assert ip in ('149.156.237.14', '149.156.237.13')
