8263055c30a17c629d0dd2e1161b2601aff172c2
lythx <lythxxx@gmail.com>
2026-08-27 18:29:55 +0200

    use port shorthand instead of explicit argument for slurm connection

--- scripts/setup_local_slurm.sh
+++ scripts/setup_local_slurm.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-FLASK_SQLALCHEMY_DATABASE_URI="sqlite:///instance/db.sqlite" poetry run yaptide/admin/db_manage.py add-cluster localhost -p 3022
+FLASK_SQLALCHEMY_DATABASE_URI="sqlite:///instance/db.sqlite" poetry run yaptide/admin/db_manage.py add-cluster localhost:3022
--- yaptide/batch/batch_methods.py
+++ yaptide/batch/batch_methods.py
@@ -61,7 +61,6 @@ def get_connection(user: KeycloakUserModel, cluster: ClusterModel) -> Connection
 
     con = Connection(
         host=f"{user.username}@{cluster.cluster_name}",
-        port=cluster.port,
         connect_kwargs={"pkey": pkey, "allow_agent": False, "look_for_keys": False},
     )
     return con
